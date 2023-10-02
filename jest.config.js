d22385932cfc0570f0f64a65f721dda6d8a7f26e
Alejandro Mejía <alejandro.mejia@bairesdev.com>
2023-10-02 15:44:23 -0500

    Remove template files

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 // jest.config.js
 
-export default {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node'
+}
